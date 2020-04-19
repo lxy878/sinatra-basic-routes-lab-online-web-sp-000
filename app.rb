4c64796c6909c61528a760e5763fdee276575673
Xiaoyi Liu <lxy878@gmail.com>
2020-04-19 09:43:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get('/'){
+    'Hello, World!'
+  }
+
+  get '/name' do
+    'My name is someone.'
+  end
+
+  get '/hometown' do
+    'My hometown is somewhere.'
+  end
+
+  get '/favorite-song' do
+    'My favorite song is something.'
+  end
 end
